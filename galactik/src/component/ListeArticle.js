d0b5585c015b4e447ddcf6236f4bc481df704314
Kesu19 <74417754+Kesu19@users.noreply.github.com>
2023-03-24 15:27:57 +0100

    modification style

--- galactik/src/component/ListeArticle.js
+++ galactik/src/component/ListeArticle.js
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 import axios from 'axios';
 import Header from './Header';
 import { useState } from 'react';
-import { NavLink, useParams } from 'react-router-dom';
+import { Link, NavLink, useParams } from 'react-router-dom';
 import '../styles/ListeArticle.css'
 
 
@@ -11,7 +11,7 @@ const ListeArticle = () => {
   const [posts, setPosts] = useState([]);
   var ListeArticle = []
   useEffect(() => {
-    axios.get('https://g3.esiee-it.o3creative.fr/wp-json/wp/v2/posts?tags=3')
+    axios.get('https://g3.esiee-it.o3creative.fr/wp-json/wp/v2/posts?tags=3&acf_format=standard')
       .then(response => {
         setPosts(response.data.reverse());
       })
@@ -23,9 +23,22 @@ const ListeArticle = () => {
     console.log(post.tags[0])
     ListeArticle.push({
       titre: post.title.rendered,
-      slug: "/Article/" + post.slug
+      slug: "/Article/" + post.slug,
+      img : post.acf.image[0] 
     })
   })
+  
+
+  const [pageActuelle, setPageActuelle] = useState(0);
+  const élémentsParPage = 3;
+  const nbPages = Math.ceil(ListeArticle.length / élémentsParPage);
+  const indexDeDébut = pageActuelle * élémentsParPage;
+  const indexDeFin = indexDeDébut + élémentsParPage;
+  const élémentsAAfficher = ListeArticle.slice(indexDeDébut, indexDeFin);
+
+  function setPage(page) {
+    setPageActuelle(Math.max(0, Math.min(page, nbPages - 1)));
+  }
   return (
     <div>
       <Header />
@@ -35,13 +48,24 @@ const ListeArticle = () => {
         <div className="article-box">
           <div>
           {
-            ListeArticle.map(value => (
-              <div className="DivListeArticle">
-                <h2 style={{ margin: "0px" }}><NavLink className = "titreArticle" to={value.slug} dangerouslySetInnerHTML={{ __html: value.titre }}></NavLink></h2>
+            élémentsAAfficher.map(value => (
+              <div className="card">
+                <NavLink className = "titreArticle" to={value.slug} >{
+                  <div>
+                  <img src={value.img}></img>
+                  <p dangerouslySetInnerHTML={{ __html: value.titre }}></p>
+                </div>
+                }
+                </NavLink>
+
               </div>
 
             ))
           }
+          </div>
+          <div>
+            <button className='button' onClick={() => setPage(pageActuelle - 1)}>Précédent</button>
+            <button className='button' onClick={() => setPage(pageActuelle + 1)}>Suivant</button>
           </div>
         </div>
 
